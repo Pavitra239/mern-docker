4f0d175974dfe3ea4f16f5bcce7f75ff5201ba16
Pavitra Modi <pavitramodi.it@charusat.ac.in>
2025-09-04 10:04:49 +0530

    new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,25 @@
-import { Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import UsersPage from './pages/UsersPage';
+import UserDetail from './pages/UserDetail';
+import CreateUser from './pages/CreateUser';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home Page</div>,
+  },
+  {
+    path: '/users',
+    element: <UsersPage />,
+  },
+  {
+    path: '/users/:id',
+    element: <UserDetail />,
+  },
+  {
+    path: '/create-user',
+    element: <CreateUser />,
+  },
+]);
 
 const App = () => {
   return <RouterProvider router={router} />;
